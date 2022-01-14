fef662a1be7fdf1ef486d39d9708145518bc6728
skyraider11 <96916050+skyraider11@users.noreply.github.com>
2022-01-14 14:20:54 +0530

    Update link opener_and_googlesearch.py

--- link opener_and_googlesearch.py
+++ link opener_and_googlesearch.py
@@ -20,3 +20,23 @@ title=input("Enter the project title: ")
 google(title)
 #This type of content opening is wrong as even the website code is being opened
 #Also we can make the function fancy using decorators which we can do later
+
+#Import the required libraries
+from tkinter import *
+import webbrowser
+
+#Create an instance of tkinter frame
+win = Tk()
+win.geometry("750x250")
+
+#Define a callback function
+def callback(url):
+   webbrowser.open_new_tab(url)
+
+#Create a Label to display the link
+link = Label(win, text="www.tutorialspoint.com",font=('Helveticabold', 15), fg="blue", cursor="hand2")
+link.pack()
+link.bind("<Button-1>", lambda e:
+callback("http://www.tutorialspoint.com"))
+
+win.mainloop()
